c7358db96365693a6dc9df0bf1ffd51322dd4f27
Tatiana Plotnikova <plotnikova.tatiana@gmail.com>
2023-05-09 13:31:59 -0400

    heroku deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { PrismaService } from './prisma.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
 }
