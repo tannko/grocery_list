f9333a102cbb324dc659486ef4888457fcd03a94
Tatiana Plotnikova <plotnikova.tatiana@gmail.com>
2023-05-09 14:06:34 -0400

    heroku deployment

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GroceryListsModule } from './grocery-lists/grocery-lists.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [GroceryListsModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'frontend'),
+    }),
+    GroceryListsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
